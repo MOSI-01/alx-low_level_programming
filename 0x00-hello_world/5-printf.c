563693f9b23d74641dce167e1b3aa2ac64755b45
MOSI-01 <amospngetij@gmail.com>
2023-09-07 14:23:39 -0700

    hello prinf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+
+
+
+
+
+
