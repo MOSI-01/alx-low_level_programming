f54266dcf3a410fa92f5e065f997f1e934519542
MOSI-01 <amospngetij@gmail.com>
2023-09-07 14:16:11 -0700

    hello, put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,18 @@
+
 #include <stdio.h>
-/*
-*main - this is the main function 
-*Return:always 0(success)
-*/
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that prints the message.
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
-
 
 
 
