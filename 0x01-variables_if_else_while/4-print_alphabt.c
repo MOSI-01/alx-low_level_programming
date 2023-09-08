e9713f793346c16b4a41187026e232294b2d11b3
MOSI-01 <amospngetij@gmail.com>
2023-09-08 10:04:00 -0700

    alphabet qe

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that generates a random number.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i;
+
+for (i = 'a' ; i <= 'z'; i++)
+putchar(i);
+if ("i != 'q' && i != 'e' ; i++")
+putchar(i);
+putchar('\n');
+return (0);
+}
