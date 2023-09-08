8cc0ae794a6b47bae07cb9f5c5d30aa9cf071c26
MOSI-01 <amospngetij@gmail.com>
2023-09-08 14:12:04 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all lowercase letters from 'z' to 'a' in reverse order.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i;
+
+for (i = 'z'; i > 'a'; i--)
+putchar(i);
+
+putchar('\n');
+
+return (0);
+}
